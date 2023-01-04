f92d30faabe7d5d32a085a9f2eef450008fd395d
MayexS <mayexs@gmail.com>
2023-01-04 17:27:34 -0300

    Nuevos cambios prueba2

---
+++ calculadora/src/componentes/BotonClear.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../estilos/Boton.css";
+
+const BotonClear = (props) => (
+  <div className="boton-clear" onClick={props.verClic}>
+    {props.children}
+  </div>
+);
+
+export default BotonClear;
